02ba943e80fff488c8bbd5788270da378b41d1d2
Kurchaviy Vitaliy <vitalykurchaviy@yandex.ru>
2022-11-02 06:07:15 +0300

    add task_3

--- lab6/code_matlab/plot_x.m
+++ lab6/code_matlab/plot_x.m
@@ -1,21 +1,34 @@
 grid minor;
 hold on;
 
-t = out.C2x.Time;
-x_1 = out.C2x.Data(:,1);
+t = out.z1.Time;
+x_1 = out.z1.Data(:,1);
 plot(t, x_1);
 
-t_2 = out.D2w.Time;
-x_2 = out.D2w.Data(:,1);
-plot(t_2, x_2);
-
+% t_2 = out.w.Time;
+% x_2 = out.w.Data(:,1);
+% plot(t_2, x_2);
+% 
 % x_3 = out.w.Data(:,3);
 % plot(t, x_3);
 % 
+% t = out.w1.Time;
+% x_1 = out.x1.Data(:,1);
+% plot(t, x_1, '--');
+% 
+% t_2 = out.w1.Time;
+% x_2 = out.x1.Data(:,1);
+% plot(t_2, x_2, '--');
+% 
+% x_3 = out.w1.Data(:,3);
+% plot(t, x_3, '--');
+
+
 % t_4 = out.w.Time;
 % x_4 = out.w.Data(:,3);
 % plot(t, x_4);
 
 %ylabel('w')
 xlabel('t, sec')
-legend('C2x', '-D2w');
+%legend('w1', 'w2', 'w3', 'w1hat', 'w2hat', 'w3hat');
+legend('z')
